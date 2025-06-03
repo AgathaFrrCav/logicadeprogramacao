c44f24edce5f8b4f62af909cb2d6fb8926b68b2b
Agatha <agathaferreiracavalcante50@gmail.com>
2025-06-03 10:15:45 -0300

    Desafio 1. 29 de Abril.

---
+++ QuebraCabeca/Desafio1.js
@@ -0,0 +1,85 @@
+//Questão 1
+//Esse bloco declara as váriavéis de nome, idade e categoria. O let resultado será acionado ao final para mostar a
+váriavél idade +5 números como resultado.
+/*let idade = 27;
+let categoria
+const nome = " Fernanda"
+let resultado = idade + 5;
+console.log("Nome:" + nome)
+//Se a número for menor do que 12, a saída será "Criança".
+if(idade <= 12){
+categoria = "Criança"
+console.log("Categoria: " + categoria)}
+//Se a número for menor do que 18 e maior d que 12, a saída será "Adolescente".
+else if(idade >= 12 && idade <= 18){
+categoria = "Adolescente"
+console.log("Categoria" + categoria)}
+//Se a número for maior que 18 e menor que 60, a saída será "Adulta".
+else if(idade >= 18 && idade <= 60){
+categoria = " Adulta"
+console.log("Categoria:" + categoria)}
+//Se a número for maior 60 a categoria será "Idosa."
+else if (idade >= 60){
+categoria = "Idosa"
+console.log("Categoria: " + categoria)}
+
+console.log("Idade: " + idade)
+console.log("Resultado final: " + resultado)*/
+
+//Questão 2
+//Declaração da idade
+/*let idade = 25;
+
+//Se a idade for maior do que 16 e menor do que 18, a saída será "voto facultativo".
+if(idade >= 16 && idade < 18){
+
+console.log("Voto facultativo.")}
+
+//Se a idade for maior de 18 a saída será "Voto obrigatório."
+else if(idade >= 18){
+console.log("Voto obrigatório.")}
+
+//Se a idade for maior de 70 a saída será "Voto facultativo."
+else if(idade >= 70){
+console.log("Voto facultativo.")}
+
+//Se a idade for menor de 18 asaída será "Não vota."
+else if(idade >= 18){
+console.log("Não vota.")}*/
+
+//Questão 3
+/*let numero = -5;
+
+if(numero <= 0){
+console.log("Negativo")}
+
+else if (numero >= 0){
+console.log("Positivo")}*/
+
+//Questão 4
+//Declara qual combustivel está sendo usado. Os próximos blocos mostrarão a saída com a escolha.
+/*let combustivel = "Gasolina"
+
+if (combustivel === "Gasolina"){
+console.log("Você escolheu Gasolina")}
+
+else if (combustivel === "Diesel"){
+console.log("Você escolheu Diesel")}
+
+else if (combustivel === "Álcool"){
+onsole.log("Você escolheu Álcool")}*/
+
+//Questão 5
+//O código indica o limite de velocidade com sua adequação.
+/*let velocidade = 75
+
+if (velocidade <= 60) {
+console.log("No limite");}
+
+else if (velocidade <= 80) {
+console.log("Atenção")}
+
+else{
+console.log("Multado");
+}
+*/
