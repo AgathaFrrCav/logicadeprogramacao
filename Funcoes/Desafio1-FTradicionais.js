3f6bfc339addd79870b65106485595c359379321
Agatha <agathaferreiracavalcante50@gmail.com>
2025-06-03 10:19:48 -0300

    Variavéis JS. 6 de Maio.

---
+++ Funcoes/Desafio1-FTradicionais.js
@@ -0,0 +1,164 @@
+//Questão 1
+/*function saudacao(){
+  console.log("Olá Mundo");
+}
+saudacao()
+*/
+
+//Questão 2
+/*let nome = prompt("Insira o seu primeiro nome: ")
+let sobrenome = prompt("Insira o seu sobrenome: ")
+
+function nomecompleto(){
+    let nomecompleto = nome + sobrenome
+    console.log("Nome completo: " + nomecompleto)
+}
+nomecompleto()*/
+
+//Questão 3
+/*let idade = Number(prompt("Digite sua idade: "))
+function mostrarIdade(){
+    console.log("Sua idade é: " + idade)
+}
+mostrarIdade()
+*/
+
+//Questão 4
+/*let num1 = 8;
+let num2 = 8;
+
+function soma() {
+    console.log("A soma dos números é: " + (num1 + num2));
+}
+soma();
+*/
+
+//Questão 5
+/*let mensagem = ("Você consegue!")
+function mostrarMensagem(){
+console.log(mensagem)
+}
+mostrarMensagem()
+*/
+
+//Questão 6
+/*let data = ("6 de Maio de 2025")
+function mostrarData(){
+    console.log(data)
+}
+mostrarData()
+*/
+
+//Questão 7
+/*let cidade = ("São Paulo")
+function mostrarCidade(){
+    console.log(cidade)
+}
+mostrarCidade()
+*/
+
+//Questão 8 
+/*let nome = prompt("Insira o seu nome: ")
+function exibir(){
+    console.log("Bem vindo(a) " + nome)
+}
+exibir()
+*/
+
+//Questão 9
+/*let num1 = Number(prompt("Insira o primeiro número: "))
+let num2 = Number(prompt("Insira o segundo número: "))
+function subtracao(){
+    console.log("A subtração dos números é:", num1 - num2)
+}
+subtracao()
+*/
+ 
+ //Questão 10
+ /*let animal = ("Zebra")
+ function animalFavorito(){
+     console.log(animal)
+ }
+animalFavorito()
+*/
+  
+  //Questão 11
+ /* let função = ("Estou aprendendo JavaScript!")
+  function exibir(){
+      console.log(função)
+  }
+ exibir()
+*/
+
+//Questão 12
+/*let num = Number(prompt("Insira um número: "))
+function mult(){
+    console.log("O triplo do seu número é: ", num * 3)
+}
+mult()
+*/
+
+//Questão 13
+/*let nome = prompt("Coloque o seu nome: ")
+let idade = prompt("Coloque a sua idade: ")
+function nomeIdade(){
+    console.log("Nome: " + nome + "\nIdade: " + idade)
+}
+nomeIdade()
+*/
+
+//Questão 14
+/*let numeros = ("1,2,3,4,5,6,7,8,9,10")
+function num(){
+    console.log(numeros)
+}
+num()
+*/
+
+//Questão 15 
+/*let aula = ("Aula de funções iniciada!")
+function exibir(){
+    console.log(aula)
+}
+exibir()
+*/
+
+//Questão 16
+/*let num = Number(prompt("Insira um número: "))
+function escolha(){
+    console.log("Você esolheu o número: " + num)
+}
+escolha()
+*/
+
+//Questão 17
+/*let curso = ("Ánalise e desenvolvimento de sistemas")
+function nome(){
+    console.log(curso)
+}
+nome()
+*/
+
+//Questão 18
+/*let chamada = prompt("Insira o seu nome: ")
+function nome(){
+    console.log(chamada + " está na chamada")
+}
+nome()
+*/
+
+//Questão 19
+/*let cor = prompt("Insira uma cor: ")
+function corFavorita(){
+    console.log("Sua cor favorita é: " + cor)
+}
+corFavorita()
+*/
+
+//Questão 20
+/*let pais = prompt("Digite um país: ")
+function mora(){
+    console.log("Você mora em: " + pais)
+}
+mora()
+*/
